758fec834e91ad79f7dd47251087cc75fce1cb0f
BonfaceKilz <me@bonfacemunyoki.com>
2022-01-14 09:37:31 +0300

    Initial Commit

---
+++ manifest.scm
@@ -0,0 +1,11 @@
+(specifications->manifest
+ '("python-matplotlib"
+   "python-notebook"
+   "python-numpy"
+   "python-pandas"
+   "python-scikit-learn"
+   "python-scipy"
+   "python-seaborn"
+   "python-statsmodels"
+   "python-wrapper"
+   "texlive-bin"))
